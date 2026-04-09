5b7bf5d188ab59cbdd14113d280afe9d11c678d0
Jocelyn Zhang <jocelynzhang@utexas.edu>
2026-04-09 17:35:38 -0500

    more 9b files (evaluated with tc) + some 2b files

--- scripts/run_all_gemma_it.sh
+++ scripts/run_all_gemma_it.sh
@@ -91,35 +91,35 @@ cd /datastor2/jocelyn/rankalign/scripts
 # run 1 24 ./run_hypernym_concat.sh 1 --model google/gemma-2-9b-it --task hypernym-concat-bananas-to-dogs-double --tc --variant sft       --semi-mode semi --semi-ratio 0.1
 # run 1 24 ./run_hypernym_concat.sh 1 --model google/gemma-2-9b-it --task hypernym-concat-bananas-to-dogs-double --tc --variant all_terms --semi-mode semi --semi-ratio 0.1
 
-# Gemma-2-2B-IT | PlausibleQA | N/A
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant base --semi-mode none
+# # Gemma-2-2B-IT | PlausibleQA | N/A
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant base --semi-mode none
 
-# Plain | Label-only
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant pref_only --semi-mode labelonly --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant sft       --semi-mode labelonly --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant all_terms --semi-mode labelonly --semi-ratio 0.1
+# # Plain | Label-only
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant pref_only --semi-mode labelonly --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant sft       --semi-mode labelonly --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant all_terms --semi-mode labelonly --semi-ratio 0.1
 
-# Plain | Semisupervised
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant pref_only --semi-mode semi --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant sft       --semi-mode semi --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant all_terms --semi-mode semi --semi-ratio 0.1
+# # Plain | Semisupervised
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant pref_only --semi-mode semi --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant sft       --semi-mode semi --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --variant all_terms --semi-mode semi --semi-ratio 0.1
 
-# TC-self | Label-only
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant pref_only --semi-mode labelonly --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant all_terms --semi-mode labelonly --semi-ratio 0.1
+# # TC-self | Label-only
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant pref_only --semi-mode labelonly --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant all_terms --semi-mode labelonly --semi-ratio 0.1
 
-# TC-self | Semisupervised
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant pref_only --semi-mode semi --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant sft       --semi-mode semi --semi-ratio 0.1
-run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant all_terms --semi-mode semi --semi-ratio 0.1
+# # TC-self | Semisupervised
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant pref_only --semi-mode semi --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant sft       --semi-mode semi --semi-ratio 0.1
+# run 1 10 ./run_plausibleqa.sh 1 --model google/gemma-2-2b-it --task plausibleqa --tc --variant all_terms --semi-mode semi --semi-ratio 0.1
 
-# Gemma-2-2B-IT | AmbigQA | N/A
-run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant base --semi-mode none
+# # Gemma-2-2B-IT | AmbigQA | N/A
+# run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant base --semi-mode none
 
-# Plain | Label-only
-run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant pref_only --semi-mode labelonly --semi-ratio 0.1
-run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant sft       --semi-mode labelonly --semi-ratio 0.1
-run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant all_terms --semi-mode labelonly --semi-ratio 0.1
+# # Plain | Label-only
+# run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant pref_only --semi-mode labelonly --semi-ratio 0.1
+# run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant sft       --semi-mode labelonly --semi-ratio 0.1
+# run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant all_terms --semi-mode labelonly --semi-ratio 0.1
 
 # # Plain | Semisupervised
 # run 1 10 ./run_ambigqa.sh 1 --model google/gemma-2-2b-it --task ambigqa --variant pref_only --semi-mode semi --semi-ratio 0.1
